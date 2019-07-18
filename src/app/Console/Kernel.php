17f0be5db18ac18aec958b3edbf1d4a66c08c183
Jackie Meng <mengjq08@gmail.com>
2019-07-18 23:01:26 +0800

    fix schedule

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('bc:sales:report')
-                  ->everyMinute();
+                  ->daily()->at('00:02');
     }
 
     /**
